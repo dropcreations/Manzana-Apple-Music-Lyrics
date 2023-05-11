64bc1bfbadde735a5898cbebaf4f68a1e29b86cf
dropcodes <97192664+dropcreations@users.noreply.github.com>
2023-05-11 11:20:38 +0530

    Update applemusic.py

--- applemusic.py
+++ applemusic.py
@@ -23,7 +23,7 @@ class AppleMusic(object):
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
         }
 
-        self.sync_points = syncPoints
+        self.sync_points = int(syncPoints)
         self.__get_access_token()
         self.__load_cookies()
 
